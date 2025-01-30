e2696cea9473124eb695693581e9078c8d43d5e4
Gaël Bigot <gbigotraingeard@gmail.com>
2025-01-31 00:58:43 +0100

    Changed memory from stack to .bss block

--- bfc.c
+++ bfc.c
@@ -32,11 +32,8 @@ int main(int argc, char* argv[]){
     // Usual asm stuff
     fprintf(output_file, "section .text\nglobal _start\n_start:\n");
 
-    /* Setting ~60000 bytes of the heap to 0, then setting stack pointer at ~30000
-    Note : the brainf*ck "standard" guarantees at least 30000 bytes of memory at positive adresses,
-    but does not guarantee negative adresses to be accessible.
-    This compiler also gives ~30000 bytes of negative memory for conveniency.*/
-    fprintf(output_file, "\tmov rcx,0FFFFh\n\tloop:\n\tmov byte [rsp],0\n\tdec rsp\n\tdec rcx\n\tjnz loop\n\tadd rsp,07FFFh\n");
+    /* Setting rax as memory pointer. 64k bytes of memory is alocated in .bss section */
+    fprintf(output_file, "\tmov rax, mem\n\tadd rax, 0x7FFF\n");
 
 
     bool do_incr;
@@ -102,45 +99,45 @@ int main(int argc, char* argv[]){
         if (do_incr && incr_acc){
             // This if statement is purely cosmetic as ADD works with negative values
             if (incr_acc > 0){
-                fprintf(output_file, "\tadd byte [rsp],%d\n", incr_acc);
+                fprintf(output_file, "\tadd BYTE [rax], %d\n", incr_acc);
             } else {
-                fprintf(output_file, "\tsub byte [rsp],%d\n", -incr_acc);
+                fprintf(output_file, "\tsub BYTE [rax], %d\n", -incr_acc);
             }
             incr_acc = 0;
         }
         if (do_shift && shift_acc){
             if (shift_acc > 0){
-                fprintf(output_file, "\tsub rsp,%d\n", shift_acc);
+                fprintf(output_file, "\tsub rax, %d\n", shift_acc);
             } else {
-                fprintf(output_file, "\tadd rsp,%d\n", -shift_acc);
+                fprintf(output_file, "\tadd rax, %d\n", -shift_acc);
             }
             shift_acc = 0;
         }
         if (do_write){
             // write syscall
-            fprintf(output_file, "\tmov rdi,1\n\tmov rsi,rsp\n\tmov rdx,1\n\tmov rax,1\n\tsyscall\n");
+            fprintf(output_file, "\tpush rax\n\tmov rdi, 1\n\tmov rsi, rax\n\tmov rdx, 1\n\tmov rax, 1\n\tsyscall\n\tpop rax\n");
             do_write =false;
         }
         if (do_read){
             // read syscall
-            fprintf(output_file, "\tmov rdi,0\n\tmov rsi,rsp\n\tmov rdx,1\n\tmov rax,0\n\tsyscall\n");
+            fprintf(output_file, "\tpush rax\n\tmov rdi, 0\n\tmov rsi, rax\n\tmov rdx, 1\n\tmov rax, 0\n\tsyscall\n\tpop rax\n");
             do_read = false;
         }
         if (do_new_loop){
             loop_stack = push(loop_stack, loop_index);
-            fprintf(output_file, "\tcmp byte [rsp],0\n\tje loop%d_end\n\tloop%d:\n", loop_index, loop_index);
+            fprintf(output_file, "\tcmp BYTE [rax], 0\n\tje loop%d_end\n\tloop%d:\n", loop_index, loop_index);
             loop_index++;
             do_new_loop = false;
         }
         if (do_close_loop){
             loop_stack = pop(loop_stack, &curr_loop);
-            fprintf(output_file, "\tcmp byte [rsp],0\n\tjne loop%d\n\tloop%d_end:\n", curr_loop, curr_loop);
+            fprintf(output_file, "\tcmp BYTE [rax], 0\n\tjne loop%d\n\tloop%d_end:\n", curr_loop, curr_loop);
             do_close_loop = false;
         }
 
     }
-    // Exit syscall
-    fprintf(output_file, "\tmov rdi,0\n\tmov rax,60\n\tsyscall");
+    // Exit syscall and .bss section
+    fprintf(output_file, "\tmov rdi, 0\n\tmov rax, 60\n\tsyscall\nsection .bss\nmem: resb 0xFFFF");
     
     return 0;
 }
